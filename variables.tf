36856e70cfd980aab497232b516d759a25c2ffbd
shaiki <shaik.urs@gmail.com>
2021-04-24 16:39:53 +0530

    Initial commit

---
+++ variables.tf
@@ -0,0 +1,6 @@
+variable "sqs_delay_seconds"{}
+variable "sqs_max_message_size"{}
+variable "sqs_message_retention_seconds"{}
+variable "sqs_receive_wait_time_seconds"{}
+variable "sqs_visibility_timeout_seconds" {}
+variable "sqs_environment" {}
